04ec10b7bcaabee5198758ded89517a886d5a3db
PatrikBorzik <patrikborzik@gmail.com>
2025-11-21 23:36:22 +0100

    test input data change

--- pieces/GenerativeShapesPiece/test_example_generative_shapes_piece.py
+++ pieces/GenerativeShapesPiece/test_example_generative_shapes_piece.py
@@ -4,7 +4,9 @@ from pieces.GenerativeShapesPiece.models import InputModel
 
 def test_generate_shapes():
     inp = InputModel()
-    res = piece_dry_run("GenerativeShapesPiece", inp)
+    # Ensure we pass a JSON-serializable dict to the testing HTTP client
+    input_data = inp.model_dump() if hasattr(inp, "model_dump") else inp.dict()
+    res = piece_dry_run("GenerativeShapesPiece", input_data)
     # piece_dry_run returns a dict-like model dump
     assert isinstance(res, dict)
     assert res.get("file_path") == "shapes.png"
